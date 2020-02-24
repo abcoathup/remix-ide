3256e4ce6aeb37f48f26085b3099db618f00218f
ioedeveloper <daviddisu8@gmail.com>
2020-02-24 14:20:12 +0000

    Fixed failing gist tests for firefox

--- test-browser/commands/executeScript.js
+++ test-browser/commands/executeScript.js
@@ -2,13 +2,19 @@ const EventEmitter = require('events')
 
 class ExecuteScript extends EventEmitter {
   command (script) {
+    const runtimeBrowser = this.api.capabilities.browserName
+
     this.api
-      .clearValue('span[data-id="terminalCliInput"]')
-      .click('div[data-id="terminalCli"]')
-      .keys(script)
-      .keys(this.api.Keys.ENTER)
-      .keys(this.api.Keys.ENTER) // that's a bug... sometimes we need to press 2 times to execute a command
-      .perform(() => {
+      .clearValue('*[data-id="terminalCliInput"]')
+      .click('*[data-id="terminalCli"]')
+      if(runtimeBrowser === 'firefox') {
+        this.api.setValue('*[data-id="terminalCliInput"]', [script, this.api.Keys.ENTER, this.api.Keys.ENTER]);
+      } else {
+        this.api.keys(script)
+        .keys(this.api.Keys.ENTER)
+        .keys(this.api.Keys.ENTER) // that's a bug... sometimes we need to press 2 times to execute a command
+      }
+      this.api.perform(() => {
         this.emit('complete')
       })
     return this
